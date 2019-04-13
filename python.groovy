874351b4359f162b6bb189904dbba330f26cc557
Sean Isaac-Elder <sisaacelder@gmail.com>
2019-04-13 19:46:56 -0400

    update

--- python.groovy
+++ python.groovy
@@ -1,29 +1,5 @@
-pipeline {
-    agent any
-    stages {
-        stage('Testing') {
-            steps {
-                script {
-                    withPythonEnv('/usr/local/src/bin/python2.7') {
-                        sh """
-                            python -V && which virtualenv && ls
-                            source activate ${BUILD_TAG}
-                        """
-                    }
-                }
-            }  
-        }
-        stage('Testing1') {
-            steps {
-                script {
-                    withPythonEnv('/usr/local/src/bin/python2.7') {
-                        sh """
-                            python hello.py
-                        """
-                    }
-                }
-            }  
-        }
-    }
+withPythonEnv('python') {
+    // Uses the default system installation of Python
+    // Equivalent to withPythonEnv('/usr/bin/python') 
+    print('hello')
 }
-
