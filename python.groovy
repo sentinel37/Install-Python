73eb1ade22c81b5ed5c2d9883e6f97f3f1efefce
Sean Isaac-Elder <sisaacelder@gmail.com>
2019-04-13 01:15:50 -0400

    Updated

--- python.groovy
+++ python.groovy
@@ -1,18 +1,16 @@
-if (fileExists('requirements/preinstall.txt')) {
-    sh """
-    . .env/bin/activate
-    pip install -r requirements/preinstall.txt
-    """
-}
-...
-sh """
-. .env/bin/activate
-pip install -r requirements/test.txt
-"""
-}
-stage("Unittests") {
-    sh """
-    . .env/bin/activate
-    ./hello.py test --noinput
-    """
+pipeline {
+    agent any
+    stages {
+        stage('Testing') {
+            steps {
+                script {
+                    withPythonEnv('/usr/local/src/bin/python2.7') {
+                        sh """
+                            python -V && which virtualenv && ls
+                        """
+                    }
+                }
+            }
+        }
+    }
 }
