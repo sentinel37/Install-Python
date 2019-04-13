dc1e66879ff8a47a3d2111c0f09c397280226a24
Sean Isaac-Elder <sisaacelder@gmail.com>
2019-04-12 23:35:17 -0400

    Updated

--- python.groovy
+++ python.groovy
@@ -1,16 +1,41 @@
 pipeline {
     agent any
+
+    triggers {
+        pollSCM('*/5 * * * 1-5')
+    }
+    options {
+        skipDefaultCheckout(true)
+        // Keep the 10 most recent builds
+        buildDiscarder(logRotator(numToKeepStr: '10'))
+        timestamps()
+    }
+    environment {
+      PATH="/var/lib/jenkins/miniconda3/bin:$PATH"
+    }
+
     stages {
-        stage('Testing') {
+
+        stage ("Code pull"){
+            steps{
+                checkout scm
+            }
+        }
+        stage('Build environment') {
             steps {
-                script {
-                    withPythonEnv('/usr/local/src/bin/python2.7') {
-                        sh """
-                            python -V && which virtualenv && ls
-                        """
-                    }
-                }
+                sh '''conda create --yes -n ${BUILD_TAG} python
+                      source activate ${BUILD_TAG} 
+                      pip install -r requirements.txt
+                    '''
+            }
+        }
+        stage('Test environment') {
+            steps {
+                sh '''source activate ${BUILD_TAG} 
+                      pip list
+                      which pip
+                      which python
+                    '''
             }
         }
     }
-}
